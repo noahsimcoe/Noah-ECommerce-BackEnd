bf490e3e351b2e2fdfe8b6ba77cdba589e08aa08
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-19 08:51:22 -0500

    created fields/columns for the models in our application

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+      // TODO: validates that the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+      // TODO: validates that the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false,
+      }
+    }
   },
   {
     sequelize,
